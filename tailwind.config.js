c5ea388efcc0d19c158d7c683834272d27a46619
ramen-soft <ramen.devel@gmail.com>
2024-09-26 14:51:49 +0200

    Clean default code and install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: "selector",
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
